d6f28f0555f4663c4ff04a79a2b64f3f88f65fba
HuangYouyi <2572082773@qq.com>
2022-01-27 15:36:55 +0800

    适配火箭大小

--- src/components/BackTop.vue
+++ src/components/BackTop.vue
@@ -1,85 +1,91 @@
 <template lang="">
-  <div v-if="isShow">
+  <div v-if="isShow" class="back-top-box">
     <i class="back-top iconfont icon-huojian1 canTap " :class="{'animation-move':isAnimation}" title="回到顶部"
       @click="backToTop()"></i>
   </div>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        isShow: false,
-        isAnimation: false,
+export default {
+  data() {
+    return {
+      isShow: false,
+      isAnimation: false,
+    };
+  },
+  methods: {
+    backToTop() {
+      this.isAnimation = true;
+      let that = this;
+      console.log(document.documentElement.scrollTop);
+      let delt = document.documentElement.scrollTop / 150.0;
+      // console.log(delt);
+      let timer = setInterval(function () {
+        document.documentElement.scrollTop -= delt;
+        if (document.documentElement.scrollTop <= delt) {
+          clearInterval(timer);
+          that.isAnimation = false;
+        }
+      }, 2);
+    },
+    reload() {
+      let bodyHeight = document.body.scrollHeight;
+      let screenHeight = window.screen.height; //屏幕高度
+      console.log("bodyHeight : ", bodyHeight);
+      console.log("screenHeight : ", screenHeight);
+      if (bodyHeight > screenHeight) {
+        this.isShow = true;
+      } else {
+        this.isShow = false;
       }
     },
-    methods: {
-      backToTop() {
-        this.isAnimation = true;
-        let that = this;
-        console.log(document.documentElement.scrollTop);
-        let delt = document.documentElement.scrollTop / 150.0;
-        // console.log(delt);
-        let timer = setInterval(function () {
-          document.documentElement.scrollTop -= delt;
-          if (document.documentElement.scrollTop <= delt) {
-            clearInterval(timer);
-            that.isAnimation = false;
-          }
-        }, 2);
+  },
+  mounted() {
+    let _this = this;
+    window.addEventListener("scroll", (e) => {
+      _this.isShow = true;
+    });
+  },
+  watch: {
+    $route: {
+      handler(newValue, oldValue) {
+        this.reload();
       },
-      reload() {
-        let bodyHeight = document.body.scrollHeight;
-        let screenHeight = window.screen.height; //屏幕高度
-        console.log("bodyHeight : ", bodyHeight);
-        console.log("screenHeight : ", screenHeight);
-        if (bodyHeight > screenHeight) {
-          this.isShow = true;
-        } else {
-          this.isShow = false;
-        }
-      }
     },
-    mounted() {
-      let _this = this;
-      window.addEventListener("scroll",(e)=>{
-        _this.isShow = true;
-      })
-    },
-    watch: {
-      $route: {
-        handler(newValue, oldValue) {
-          this.reload();
-        }
-      }
-    }
-  }
-
+  },
+};
 </script>
+
+
 <style lang="">
-  .back-top {
-    position: fixed;
-    width: 5rem;
-    height: 5rem;
-    right: 5rem;
+.icon-huojian1:before {
+  font-size: 5rem ;
+}
+</style>
+
+<style >
+.back-top {
+  position: fixed;
+  width: 5rem;
+  height: 5rem;
+  right: 5rem;
+  bottom: 6rem;
+  font-size: 5rem;
+  color: red;
+}
+
+.animation-move {
+  -webkit-animation: moveUp 0.9s ease-in-out;
+  animation: moveUp 0.9s ease-in-out;
+}
+@keyframes moveUp {
+  0% {
     bottom: 6rem;
-    font-size: 5rem;
-    color: red;
   }
-
-  .animation-move {
-    -webkit-animation: moveUp 0.9s ease-in-out;
-    animation: moveUp 0.9s ease-in-out;
+  99% {
+    bottom: 120%;
   }
-  @keyframes moveUp {
-    0% {
-      bottom: 6rem;
-    }
-    99% {
-      bottom: 120%;
-    }
-    100% {
-      bottom: 6rem;
-    }
+  100% {
+    bottom: 6rem;
   }
-
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
-module.exportj = {
+module.exports = {
 	publicPath: "./"
 }
