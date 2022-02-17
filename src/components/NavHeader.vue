0f4d39f5a368f37458a580a82c13857ad19f779a
HuangYouyi <2572082773@qq.com>
2022-02-17 20:30:21 +0800

    导航条集中编辑模块为下拉列表

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -9,7 +9,6 @@
       </div>
       <div class="navbar-menu">
         <div class="navbar-start">
-
           <router-link to="/FocusCard" class="navbar-item">我的</router-link>
           <router-link to="/RecommendCard" class="navbar-item"
             >推荐</router-link
@@ -40,31 +39,66 @@
               搜索
             </button>
           </div>
-          <div class="navbar-item">
-            <button type="button" class="mybtn button is-rounded nav-line">
-              <router-link class="white" to="/EditArticle/1">
-                我要分享
-              </router-link>
-            </button>
-          </div>
-          <div class="navbar-item">
-            <button type="button" class="mybtn button is-rounded nav-line">
-              <router-link class="white" to="/EditArticle/0">
-                我要设问
-              </router-link>
-            </button>
-          </div>
         </div>
 
         <div class="navbar-end">
+          <el-dropdown>
+            <el-button
+              class="writing-button iconfont icon-tianchongxing-"
+            ></el-button>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>
+                <router-link to="/EditArticle/1">
+                  <p class="dropdown-item"><i class="iconfont icon-yongyan"></i>写文章</p>
+                </router-link>
+              </el-dropdown-item>
+              <el-dropdown-item>
+                <router-link to="/EditArticle/0">
+                  <p class="dropdown-item "><i class="iconfont icon-wenti"></i>发问题</p>
+                </router-link>
+              </el-dropdown-item>
+              <el-dropdown-item>
+                <router-link to="/EditArticle/2">
+                  <p class="dropdown-item "><i class="iconfont icon-xuanzeti"></i>设问题</p>
+                </router-link>
+              </el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
 
           <!-- 登录后的显示的user-icon -->
-          <div class="navbar-item" v-if="loginState">
-            <span
+          <el-dropdown v-if="loginState">
+            <el-button
+              class="writing-button user-bar"
+              style="font-size: 0.55rem"
+              ><i class="fas fa-user fa-3x"></i>
+            </el-button>
+
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>
+                <router-link to="/setting" >
+                  <p class="dropdown-item"><i class="fas fa-user"></i>主页</p>
+                </router-link>
+              </el-dropdown-item>
+              <el-dropdown-item>
+                <router-link to="/setting">
+                  <p class="dropdown-item"><i class="fas fa-cog"></i>设置</p>
+                </router-link>
+              </el-dropdown-item>
+              <el-dropdown-item>
+                <a @click="logout">
+                  <p class="dropdown-item"><i class="fas fa-sign-out-alt"></i>退出</p>
+                </a>
+              </el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+
+          
+          <!-- <div class="navbar-item" v-if="loginState">
+            <p
               class="navbar-link is-arrowless user-bar"
               style="font-size: 0.55rem"
-            ><i class="fas fa-user fa-3x"></i>
-            </span>
+              ><i class="fas fa-user fa-3x"></i>
+            </p>
             <div class="navbar-dropdown mydropdown">
               <router-link to="/setting" class="navbar-item"
                 ><i class="fas fa-user"></i>&nbsp;我的主页</router-link
@@ -73,20 +107,26 @@
                 ><i class="fas fa-cog"></i>&nbsp;设置</router-link
               >
               <a class="navbar-item is-clickable" @click="logout">
-                <i class="fas fa-sign-out-alt"></i>&nbsp;退出
+                <i class="fas fa-sign-out-alt"></i>&nbsp;
               </a>
             </div>
-          </div>
+          </div> -->
 
           <!-- 未登录时显示的user-icon -->
-          <router-link to="/login" class="navbar-item" v-else>
+          <!-- <router-link to="/login" class="navbar-item" v-else>
             <span class="icon" style="font-size: 0.45rem">
               <i class="fas fa-user-plus fa-3x"> </i>
             </span>
-          </router-link>
+          </router-link> -->
 
-          <div class="navbar-item"></div>
-          <div class="navbar-item"></div>
+          <el-button
+            class="writing-button user-bar"
+            style="font-size: 0.55rem"
+            @click="$router.push('/login')"
+            v-else
+          >
+            <i class="fas fa-user-plus fa-3x"></i>
+          </el-button>
         </div>
       </div>
     </nav>
@@ -141,7 +181,23 @@ export default {
 };
 </script>
 
-
+<style>
+.el-dropdown {
+  vertical-align: top;
+}
+.el-dropdown + .el-dropdown {
+  margin-left: 15px;
+}
+.el-icon-arrow-down {
+  font-size: 12px;
+}
+.el-dropdown-menu__item {
+  display: block;
+  padding: 10px;
+  margin-left: 5px;
+  margin-right: 5px;
+}
+</style>
 <style lang="css" src="@/assets/css/bulma.min.css" scoped></style>
 <style lang="css" scoped>
 @import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css";
@@ -170,9 +226,9 @@ export default {
   font-size: 14px;
 }
 
-.user-bar:hover+.mydropdown{
-  display:block;
-  height:93px;
+.user-bar:hover + .mydropdown {
+  display: block;
+  height: 93px;
 }
 
 .mydropdown {
@@ -181,26 +237,49 @@ export default {
   width: 100px;
   overflow: hidden;
   height: 0px;
-  transition:  all 1s;
+  transition: all 1s;
 }
-.mydropdown:hover{
-  display:block;
-  height:93px;
+.mydropdown:hover {
+  display: block;
+  height: 93px;
 }
-
 
 #navbar {
-  display:flex;
+  display: flex;
   padding: 0;
-  width:100%;
+  width: 100%;
 }
 
 .mybtn {
   padding: 0;
 }
 
-.navbar-menu{
-  min-width:600px;
-  height:100%
+.navbar-menu {
+  min-width: 600px;
+  height: 100%;
+  margin-right: 0!important;
+}
+
+.writing-button {
+  box-sizing: border-box;
+  height: 100%;
+  font-size: 1.5rem;
+  color: rgb(0, 0, 0);
+  background-color: hsl(0, 100%, 100%);
+  border-width: 0px;
+  opacity: 0.9;
+}
+.writing-button:hover {
+  background-color: hsl(240, 100%, 95%);
+  color: #0052cc;
+  opacity: 0.8;
+}
+
+.dropdown-item {
+  margin: 10px auto;
+  padding-left: 15px;
+  padding-right:15px;
+  min-width: 50px;
+  text-align: center;
 }
 </style>
