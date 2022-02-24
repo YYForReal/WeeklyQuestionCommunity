c46643461c5c4decb2425c65d3eb0c2f7721d5c2
HuangYouyi <2572082773@qq.com>
2022-02-24 16:12:13 +0800

    导航全局快捷键移入App.vue组件

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,17 @@
   <div id="app">
     <router-view :key="$route.path" />
     <BackTop></BackTop>
-    <MusicDisk/>
+    <MusicDisk />
   </div>
 </template>
-
 <script>
-
 import BackTop from "@/components/BackTop.vue";
 import MusicDisk from "@/components/MusicDisk.vue";
-
 export default {
   name: "App",
   components: {
     BackTop,
-    MusicDisk
+    MusicDisk,
   },
   mounted() {
     // 展示上次浏览的历史文章
@@ -37,11 +34,57 @@ export default {
         },
       });
     }
+    this.bindKeyboard();
   },
   methods: {
-    jumpToArticle(id){
-      this.$router.push("/SpecialArticle/"+id);
-    }
+    jumpToArticle(id) {
+      this.$router.push("/SpecialArticle/" + id);
+    },
+    bindKeyboard() {
+      var lastScrollY = 0;
+
+      $(function () {
+        //添加快捷键，按下'/'则聚焦到搜索栏并弹出导航栏
+        $(document).on("keyup", function (e) {
+          if (e.key == "/") {
+            $("#navbar").attr(
+              "class",
+              "navbar box is-fullhd navbar-height is-fixed-top"
+            );
+            $("#searchBox").trigger("focus");
+          }
+        });
+
+        var scrolling = false;
+        $(window).on("scroll", () => {
+          scrolling = true;
+        });
+
+        // 设置滚轮下滑时隐藏导航栏，上滑则固定导航栏
+        // 因为滚动一次滚轮会触发很多次scroll事件，所以设置300ms的延时
+
+        $(window).on("scroll", () => {
+          setInterval(() => {
+            if (scrolling) {
+              scrolling = false;
+              let scrolly = $(window).scrollTop();
+              if (scrolly >= lastScrollY) {
+                $("#navbar").attr(
+                  "class",
+                  "navbar box is-fullhd navbar-height is-hidden"
+                );
+              } else {
+                $("#navbar").attr(
+                  "class",
+                  "navbar box is-fullhd navbar-height is-fixed-top"
+                );
+              }
+              lastScrollY = scrolly;
+            }
+          }, 300);
+        });
+      });
+    },
   },
 };
 </script>
@@ -52,6 +95,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   background-color: white;
-  width:100%;
+  width: 100%;
 }
 </style>
