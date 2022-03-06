4034b70eb6be0cdcecac197d4bef03af4e23fd9f
HuangYouyi <2572082773@qq.com>
2022-03-06 12:46:22 +0800

    增加悬浮用户头像框

--- src/components/user/SmallUserBox.vue
+++ src/components/user/SmallUserBox.vue
@@ -1,15 +1,45 @@
 <template lang="">
   <div class="user-info">
-    <div class="user-img" v-if="answerFlag">
+    <div class="user-img" v-if="answerFlag" >
       <!-- 默认头像 -->
-      <img :src="answer.authorAvatar?answer.authorAvatar:'https://p3.itc.cn/images01/20211016/27d2478466b44b168a20a8255cf8334c.jpeg'">
+      <img :src="answer.authorAvatar?answer.authorAvatar:'https://p3.itc.cn/images01/20211016/27d2478466b44b168a20a8255cf8334c.jpeg'"
+      @mouseenter="isDetails = true;" @mouseleave="isDetails = false;">
     </div>
+    <transition name="el-zoom-in-top">
+      <div class="person-box" v-show="isDetails" @mouseover = "isDetails = true;" @mouseleave = "isDetails = false;">
+        <div class="user-img inner-user-img" v-if="answerFlag" >
+          <!-- 默认头像 -->
+          <img :src="answer.authorAvatar?answer.authorAvatar:'https://p3.itc.cn/images01/20211016/27d2478466b44b168a20a8255cf8334c.jpeg'"
+          @mouseenter="isDetails = true;" @mouseleave="isDetails = false;">
+        </div>
+        <div class="user-name inner-user-name" v-if="answerFlag">
+          <el-button class="focus-button" @click="handleFocus">关注</el-button>
+          <h2>{{answer.authorName==null?'匿名用户':answer.authorName}} <i class="iconfont icon-huiyuan orange"></i></h2>
+          <p class="user-description">{{answer.authorDescription==null?'':answer.authorDescription}}</p>
+        </div>
+        <div class="person-details">
+          <div>
+            <p>文章</p>
+            <h3>{{author.number}}</h3>
+          </div>
+          <div>
+            <p>问答</p>
+            <h3>{{author.number}}</h3>
+          </div>
+          <div>
+            <p>选择</p>
+            <h3>{{author.number}}</h3>
+          </div>
+        </div>
+      </div>
+    </transition>
+
     <div class="user-name" v-if="answerFlag">
       <strong>{{answer.authorName==null?'匿名用户':answer.authorName}} <i class="iconfont icon-huiyuan orange"></i>
       </strong>
       <p class="user-description">{{answer.authorDescription==null?'':answer.authorDescription}}</p>
     </div>
-
+<!-- 此处后端传值不统一，故条件判断来设置用户头信息 -->
     <div class="user-img" v-if="userFlag">
       <!-- 默认头像 -->
       <img :src="user.avatar?user.avatar:'https://p3.itc.cn/images01/20211016/27d2478466b44b168a20a8255cf8334c.jpeg'">
@@ -23,66 +53,108 @@
   </div>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        answerFlag: false,
-        userFlag: false,
-      }
+export default {
+  data() {
+    return {
+      answerFlag: false,
+      userFlag: false,
+      isDetails: false,
+      author:{number:5}
+    }
+  },
+  props: {
+    answer: {
+      type: Object,
     },
-    props: {
-      answer: {
-        type: Object,
-      },
-      user: {
-        type: Object
-      }
-    },
-    mounted() {
-      if (this.answer) {
-        this.answerFlag = true;
-        this.userFlag = false;
-      }
-      if (this.user) {
-          console.log("this user is :",this.user);
-        this.userFlag = true;
-        this.answerFlag =false;
-      }
-    },
-  }
+    user: {
+      type: Object
+    }
+  },
+  mounted() {
+    if (this.answer) {
+      this.answerFlag = true;
+      this.userFlag = false;
+    }
+    if (this.user) {
+      console.log("this user is :", this.user);
+      this.userFlag = true;
+      this.answerFlag = false;
+    }
+  },
+}
 
 </script>
-<style lang="" scoped>
-  .user-info {
-    width: 100%;
-    height: 50px;
-  }
-
-  .user-info .user-img {
+<style lang="less" scoped>
+.user-info {
+  position: relative;
+  width: 100%;
+  height: 50px;
+  .user-img {
     width: 50px;
     height: 50px;
     display: inline-block;
     margin-right: 5px;
-    border-radius: 5px;
-  }
-
-  .user-info .user-img img {
-    width: 100%;
-    height: 100%;
     border-radius: 10px;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 10px;
+    }
   }
-
-  .user-description {
-    font-size: 14px;
-  }
-
-  .user-info .user-name {
+  .user-name {
     display: inline-block;
     position: relative;
     top: -8px;
     height: 50px;
     padding: 0;
     margin: 0;
+    .user-description {
+      font-size: 14px;
+    }
   }
+  .person-box {
+    position: absolute;
+    top: -10px;
+    width: 400px;
+    height: 200px;
+    background-color: rgb(159, 216, 238);
+    opacity: 0.98;
+    z-index: 999;
+    animation: showIn 1s;
+    .inner-user-img {
+      width: 80px;
+      height: 80px;
+    }
+    .inner-user-name {
+      max-width:300px;
+      text-overflow: ellipsis;
+      .focus-button{
+        position: absolute;
+        right: 0;
+        top: 10px;
+        z-index: 9999;
+      }
+      h2 {
+        display: block;
+        color: rgb(162, 0, 255);
+        width: 300px;
+      }
+    }
+    .person-details {
+      display: flex;
+      justify-content: space-around;
+      margin-top: 10px;
+      text-align: center;
+    }
+  }
+}
 
+@keyframes showIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 100%;
+  }
+}
 </style>
