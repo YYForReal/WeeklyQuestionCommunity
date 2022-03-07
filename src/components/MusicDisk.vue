cde1b16ea2ae1a32aa8cbfcfe441d1d2d0aec100
HuangYouyi <2572082773@qq.com>
2022-03-07 13:03:47 +0800

    增加音乐列表

--- src/components/MusicDisk.vue
+++ src/components/MusicDisk.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="music-box" :class="{ rotating: playing }" @click="handleClick()">
-    <img src="https://source.acexy.cn/view/X9Fw0XQ" onerror="this.src='https://source.acexy.cn/view/X9Fw0XQ'" />
-    <audio ref="audioRef" :src="source" autoplay></audio>
+    <img
+      src="https://source.acexy.cn/view/X9Fw0XQ"
+      onerror="this.src='https://source.acexy.cn/view/X9Fw0XQ'"
+    />
+    <audio ref="audioRef" :src="sources[nowIndex]" autoplay></audio>
   </div>
 </template>
 <script>
@@ -11,28 +14,43 @@ export default {
       playing: true,
       // imgSrc: "https://source.acexy.cn/view/X9Fw0XQ",
       // errorImage: 'this.src="' + require('https://source.acexy.cn/view/X9Fw0XQ') + '"'  ,
-      source: "http://music.163.com/song/media/outer/url?id=2081057.mp3",
+      // source: "http://music.163.com/song/media/outer/url?id=2081057.mp3",
+      nowIndex:0,
+      sources: [
+        "http://music.163.com/song/media/outer/url?id=1841231031.mp3",//君子行
+        "http://music.163.com/song/media/outer/url?id=1911689798.mp3",//还是笨小孩
+        "http://music.163.com/song/media/outer/url?id=41630480.mp3",//Liberators
+        "http://music.163.com/song/media/outer/url?id=41630476.mp3",//Freedom Is Ours
+      ]
     };
   },
-  mounted(){
-      try{
-        this.$refs.audioRef.oncanplay = ()=>{
-          this.$refs.audioRef.volume = 0.5;
-        }
-        console.log(this.$refs);
-      }catch(err){
-        console.log("set audio volume error",err);
-      }
+  created(){
+    this.nowIndex = Math.floor(Math.random()*this.sources.length)
   },
-  methods:{
+  mounted() {
+    try {
+      this.$refs.audioRef.oncanplay = () => {
+        this.$refs.audioRef.volume = 0.4;
+      }
+      this.$refs.audioRef.onended = ()=>{
+        this.nowIndex = (this.nowIndex+1)%this.sources.length;
+        this.$refs.audioRef.load();
+      }
+      console.log(this.$refs);
+
+    } catch (err) {
+      console.log("set audio volume error", err);
+    }
+  },
+  methods: {
     handleClick() {
-        this.playing = !this.playing;
-        // if(this.playing)
-        if(this.playing){
-            this.$refs.audioRef.play();
-        }else{
-            this.$refs.audioRef.pause();
-        }
+      this.playing = !this.playing;
+      // if(this.playing)
+      if (this.playing) {
+        this.$refs.audioRef.play();
+      } else {
+        this.$refs.audioRef.pause();
+      }
     },
   }
 };
@@ -45,7 +63,7 @@ export default {
   left: 2rem;
   bottom: 2rem;
   border-radius: 50%;
-  cursor:pointer;
+  cursor: pointer;
   img {
     width: 100%;
     height: 100%;
